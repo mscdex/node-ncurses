14f30dfc720c53a14326852b9146a72c301c7d7d
Brian <mscdex@gmail.com>
2010-05-13 08:26:51 -0400

    Fixed typo in readme and added private messaging, action (/me), and channel name and topic display to the irc client example.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 node-curses is a node.js binding for ncurses.
 
-<b>You will need the ncurses++ C++ binding installed in addition to ncurses.<b>
+<b>You will need the ncurses++ C++ binding installed in addition to ncurses.</b>
 Currently, if you don't have ncurses++ installed, you'll get build errors, with one of the first ones being:
 	../ncurses.cc:9:21: error: cursesp.h: No such file or directory
 
--- irc.client.js
+++ irc.client.js
@@ -1,4 +1,16 @@
-var nc = require('./ncurses'), consts = require('./ncconsts'), irc = require('./irc.lib');
+var nc = require('./ncurses'), consts = require('./ncconsts'), irc = require('./irc.lib'), sys = require('sys');
+
+function updateTopic(topic) {
+	var curx=win.curx, cury=win.cury;
+	topic = "" + topic;
+	if (topic.length > 0) {
+		win.cursor(0, 0);
+		win.clrtoeol();
+		win.addstr(topic, win.cols);
+		win.cursor(cury, curx);
+	}
+	win.refresh();
+}
 
 function getTimestamp() {
 	var time = new Date();
@@ -8,6 +20,7 @@ function getTimestamp() {
 }
 
 function appendLine(message) {
+	var curx=win.curx;
 	win.scroll(1);
 	win.cursor(win.height-3, 0);
 	win.print("[" + getTimestamp() + "] ");
@@ -21,12 +34,11 @@ function appendLine(message) {
 	win.print("" + message);
 	if (arguments.length > 2 && arguments[2] && win.hasColors)
 		win.attrset(win.colorPair(0));
-	if (arguments.length > 3)
-		win.cursor(win.height-1, arguments[3]);
-	else {
+	if (arguments.length > 3) {
 		win.cursor(win.height-1, 0);
-		win.deleteln();
-	}
+		win.clrtoeol();
+	} else
+		win.cursor(win.height-1, curx);
 	win.refresh();
 }
 
@@ -41,13 +53,23 @@ function cleanup() {
 
 var win = new nc.ncWindow();
 win.addListener('inputLine', function (str) {
-	if (str == "/quit") {
+	if (str.substr(0, 4) == "/msg") {
+		str = str.substr(5);
+		var recipient = str.substr(0, str.indexOf(' '));
+		var msg = str.substr(str.indexOf(' ')+1);
+		appendLine("to(" + recipient + "): " + msg, null, null, true);
+		client.send('PRIVMSG', recipient, ':' + msg);
+	} else if (str.substr(0, 3) == "/me") {
+		str = str.substr(4);
+		appendLine("* " + nick + " " + str, null, null, true);
+		client.send('PRIVMSG', channel, ':' + String.fromCharCode(1) + "ACTION " + str + String.fromCharCode(1));
+	} else if (str == "/quit") {
 		client.send('QUIT');
 		client.disconnect();
 		cleanup();
 	} else if (!isNaN(str.charCodeAt(0))) {
-		appendLine("<" + nick + ">: " + str)
-		client.send('PRIVMSG', channel, ':' + str)
+		appendLine("<" + nick + ">: " + str, null, null, true);
+		client.send('PRIVMSG', channel, ':' + str);
 	}
 });
 
@@ -70,41 +92,51 @@ win.clear();
 win.echo = true;
 win.scrollok(true);
 win.hline(win.height-2, 0, win.width);
-win.setscrreg(0, win.height-3);
+win.setscrreg(1, win.height-3); // Leave one line at the top for the channel name and optional topic
 win.cursor(win.height-1, 0);
 win.refresh();
 
-appendLine("Connecting ...", null, null, win.curx);
+appendLine("Connecting ...");
 
 var client = new irc.Client(server, port);
 client.connect(nick, username, realname);
 
 client.addListener('001', function() { // server 'welcome message' event
-  appendLine("Connected!", null, null, win.curx);
-  this.send('JOIN', channel);
+	appendLine("Connected!");
+	this.send('JOIN', channel);
 });
 
-var onJoinChan = function() { appendLine("Joined channel " + channel, null, null, win.curx); };
+var onJoinChan = function(prefix, usr, chnl, topic) { appendLine("Joined channel " + chnl); updateTopic("[" + chnl + "] " + topic); };
 client.addListener('331', onJoinChan); // successful join, 'no topic set' event
 client.addListener('332', onJoinChan); // successful join, 'topic set' event
 
 client.addListener('JOIN', function(prefix) {
-  appendLine(irc.user(prefix) + " has joined the channel", true, COLOR_JOIN, win.curx);
+	appendLine(irc.user(prefix) + " has joined the channel", true, COLOR_JOIN);
 });
 
 client.addListener('PART', function(prefix) {
-  appendLine(irc.user(prefix) + " has left the channel", true, COLOR_PART,win.curx);
+	appendLine(irc.user(prefix) + " has left the channel", true, COLOR_PART);
 });
 
 client.addListener('QUIT', function(prefix) {
-  appendLine(irc.user(prefix) + " has quit IRC", true, COLOR_QUIT, win.curx);
+	appendLine(irc.user(prefix) + " has quit IRC", true, COLOR_QUIT);
 });
 
 client.addListener('DISCONNECT', function() {
-  appendLine("Disconnected.", null, null, win.curx);
-  setTimeout(cleanup(), 3000);
+	appendLine("Disconnected.");
+	setTimeout(cleanup(), 3000);
 });
 
-client.addListener('PRIVMSG', function(prefix, channel, text) {
-  appendLine("<" + irc.user(prefix) + ">: " + text, null, null, win.curx);
+client.addListener('PRIVMSG', function(prefix, from, text) {
+	if (text.charCodeAt(0) == 1) {
+		// CTCP message
+		var payload = text.substring(1, text.length-1);
+		if (payload.substr(0, 6) == "ACTION")
+			appendLine("* " + irc.user(prefix) + " " + payload.substr(7));
+		else
+			appendLine("CTCP payload: " + payload);
+	} else if (from[0] == "#")
+		appendLine("<" + irc.user(prefix) + ">: " + text);
+	else
+		appendLine("from(" + irc.user(prefix) + "): " + text);
 });
